1f86ea4fa0efb0bbe0fb4b65ab534726f1c6f177
max-grzanna <grzannamax@gmail.com>
2023-04-28 16:57:52 +0200

    fix url in dataset deployment

--- anthology.py
+++ anthology.py
@@ -4,7 +4,7 @@ from typing import NamedTuple, Dict
 from ir_datasets.util.download import RequestsDownload
 from ir_datasets.datasets.base import Dataset
 
-DATASET_URL = "https://raw.githubusercontent.com/max-grzanna/data/processed"
+DATASET_URL = "https://raw.githubusercontent.com/max-grzanna/Anthology-Search-Engine/main/data/processed/"
 
 
 class AnthologyDocument(NamedTuple):
