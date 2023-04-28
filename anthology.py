a68fbb3180972675dcd2ef8378ead4919de2d578
max-grzanna <grzannamax@gmail.com>
2023-04-28 18:35:47 +0200

    added len of of title key

--- anthology.py
+++ anthology.py
@@ -4,7 +4,7 @@ from typing import NamedTuple, Dict
 from ir_datasets.util.download import RequestsDownload
 from ir_datasets.datasets.base import Dataset
 
-DATASET_URL = "https://raw.githubusercontent.com/max-grzanna/Anthology-Search-Engine/main/data/processed/"
+DATASET_URL = 'https://raw.githubusercontent.com/max-grzanna/Anthology-Search-Engine/main/data/processed/'
 
 
 class AnthologyDocument(NamedTuple):
@@ -17,7 +17,7 @@ class AnthologyDocument(NamedTuple):
 
 
 ir_datasets.registry.register(
-    "anthologies",
+    'anthologies',
     Dataset(
         JsonlDocs(
             ir_datasets.util.Download(
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 tira-run \
-    --output-directory ${PWD}/data \
+    --output-directory ${PWD}/data/ \
     --image anthology-ir-dataset \
     --allow-network true \
     --command '/irds_cli.sh --ir_datasets_id anthologies --output_dataset_path $outputDir'
