4c656a22bc434f4148ab0959e85124a1ac40b96d
maxto <max.toepfer.taucha@gmail.com>
2023-06-20 12:03:05 +0200

    milestone3

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,9 @@ docker build -t ir-lab-milestone-03-sdm .
 tira-run --input-directory ${PWD}/iranthology-dataset-tira --output-directory ${PWD}/bm25-sdm-output --image ir-lab-milestone-03-sdm --allow-network true --command '/workspace/run-pyterrier-notebook.py --input $inputDataset --output $outputDir --notebook /workspace/full-rank-pipeline-sdm.ipynb'
 tira-run --output-directory ${PWD}/bm25-sdm-output --image memory-qrels --allow-network true --command 'diffir --dataset iranthology-memory --web $outputDir/run.txt > $outputDir/run.html'
 tira-run --input-directory ${PWD}/bm25-sdm-output --image memory-qrels --command 'ir_measures iranthology-memory $inputDataset/run.txt nDCG@10 MRR P@10 Recall@100'
+
+# milestone-03-multi-field
+docker build -t ir-lab-milestone-03-multi-field .
+tira-run --input-directory ${PWD}/iranthology-dataset-tira --output-directory ${PWD}/bm25-multi-field-output --image ir-lab-milestone-03-multi-field --allow-network true --command '/workspace/run-pyterrier-notebook.py --input $inputDataset --output $outputDir --notebook /workspace/full-rank-pipeline-multi-field.ipynb'
+tira-run --output-directory ${PWD}/bm25-multi-field-output --image memory-qrels --allow-network true --command 'diffir --dataset iranthology-memory --web $outputDir/run.txt > $outputDir/run.html'
+tira-run --input-directory ${PWD}/bm25-multi-field-output --image memory-qrels --command 'ir_measures iranthology-memory $inputDataset/run.txt nDCG@10 MRR P@10 Recall@100'
