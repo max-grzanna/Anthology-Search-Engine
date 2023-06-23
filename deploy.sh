90aab57fb03e58d6e521be3aec824be6108b784a
Julian Teichmann <Julianteichmann@gmx.de>
2023-06-23 16:39:38 +0200

    Hinzufügen tfidf>>dph

--- deploy.sh
+++ deploy.sh
@@ -18,3 +18,13 @@ docker build -t ir-lab-milestone-03-multi-field .
 tira-run --input-directory ${PWD}/iranthology-dataset-tira --output-directory ${PWD}/bm25-multi-field-output --image ir-lab-milestone-03-multi-field --allow-network true --command '/workspace/run-pyterrier-notebook.py --input $inputDataset --output $outputDir --notebook /workspace/full-rank-pipeline-multi-field.ipynb'
 tira-run --output-directory ${PWD}/bm25-multi-field-output --image memory-qrels --allow-network true --command 'diffir --dataset iranthology-memory --web $outputDir/run.txt > $outputDir/run.html'
 tira-run --input-directory ${PWD}/bm25-multi-field-output --image memory-qrels --command 'ir_measures iranthology-memory $inputDataset/run.txt nDCG@10 MRR P@10 Recall@100'
+
+# milestone-03-tfidf-dph
+docker build -t ir-lab-milestone-03-dph-tfidf .
+tira-run --input-directory ${PWD}/iranthology-dataset-tira --output-directory ${PWD}/bm25-dph-tfidf-output --image ir-lab-milestone-03-dph-tfidf --allow-network true --command '/workspace/run-pyterrier-notebook.py --input $inputDataset --output $outputDir --notebook /workspace/full-rank-pipeline-dph-tfidf.ipynb'
+tira-run --output-directory ${PWD}/bm25-dph-tfidf-output --image memory-qrels --allow-network true --command 'diffir --dataset iranthology-memory --web $outputDir/run.txt > $outputDir/run.html'
+tira-run --input-directory ${PWD}/bm25-dph-tfidf-output --image memory-qrels --command 'ir_measures iranthology-memory $inputDataset/run.txt nDCG@10 MRR P@10 Recall@100'
+
+#docker pushen
+docker build -t registry.webis.de/code-research/tira/tira-user-ir-lab-sose-2023-memory/ir-lab-milestone-03-dph-tfidf:0.0.1 .
+docker push registry.webis.de/code-research/tira/tira-user-ir-lab-sose-2023-memory/ir-lab-milestone-03-dph-tfidf:0.0.1
